e85f83bf7aeed58c9b66ca7d2fc35ba107b0a091
tinypinglite <tinyping@protonmail.com>
2026-09-11 13:12:45 -0400

    docs: remove obsolete v0.6.0 upgrade guide

--- wiki/.vitepress/config.ts
+++ wiki/.vitepress/config.ts
@@ -21,7 +21,6 @@ export default defineConfig({
             { text: '快速开始', link: '/guide/quick-start' },
             { text: '开源插件', link: '/guide/plugins' },
             { text: '插件开发', link: '/guide/plugin-development' },
-            { text: '升级现有部署', link: '/guide/upgrade' },
             { text: '配置说明', link: '/guide/config' },
             { text: '进阶部署', link: '/guide/docker' },
             { text: '后台任务', link: '/guide/tasks' },
